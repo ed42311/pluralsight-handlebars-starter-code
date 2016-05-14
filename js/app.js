32667a7d72e3ccf2930e3369a7b8bdd49ab0d036
ed42311 <ed42311@gmail.com>
2016-05-14 14:35:50 -0600

    completed course, all working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 (function() {
 
+  registerPartials();
   renderPage();
   renderDogs();
+
+  function registerPartials() {
+    Handlebars.registerPartial('dog', $('#dog-template').html());
+  }
 
   function renderPage() {
     var template = $('#index-template').html(),
@@ -10,16 +15,44 @@
       $('#main').html(rendered);
       $('#languageSwitch').click(function() {
         DogPack.switchLanguage();
-      });
+      });   
   }
 
   function renderDogs() {
     var template = $('#dogs-template').html(),
         compiled = Handlebars.compile(template),
-        rendered = compiled({ dogs: DogPack.dogs, language: window.language })
+        filteredDogs = DogPack.getFilteredDogs(DogPack.dogs),
+        rendered = compiled({ 
+          dogs: DogPack.getPaginatedDogs(filteredDogs), 
+          language: window.language
+        });
     $('#theDogs').html(rendered);
     attachDogButtons();
+    renderPages(filteredDogs);
+    renderScore();
   } 
+
+  function renderScore() {
+    var template = $('#score-template').html(),
+        compiled = Handlebars.compile(template),
+        rendered = compiled({
+          dogs: DogPack.dogs,
+          language: window.language
+        })
+    $('#score').html(rendered);
+    $('#score').find('small').click(function() {
+        DogPack.clearDogs();
+        window.location.href = '?' + 
+          Handlebars.helpers.getLanguageFilter(window.language.langId);
+      });
+  }
+
+  function renderPages(dogs) {
+    var template = $('#page-template').html(),
+        compiled = Handlebars.compile(template),
+        rendered = compiled({ dogs: dogs});
+    $('#pagination').html(rendered);
+  }
 
   function attachDogButtons() {
     $('.dog-button').click(function() {
